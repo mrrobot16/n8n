93e28eb7192442e5e361722d0de148bdae135bf4
Danny Martini <danny@n8n.io>
2024-08-01 19:40:27 +0200

    clean up comments again

--- packages/cli/src/license.ts
+++ packages/cli/src/license.ts
@@ -236,7 +236,6 @@ export class License {
 	}
 
 	isFeatureEnabled(feature: BooleanLicenseFeature) {
-		return true;
 		return this.manager?.hasFeatureEnabled(feature) ?? false;
 	}
 
@@ -377,7 +376,6 @@ export class License {
 	}
 
 	getTeamProjectLimit() {
-		return UNLIMITED_LICENSE_QUOTA;
 		return this.getFeatureValue(LICENSE_QUOTAS.TEAM_PROJECT_LIMIT) ?? 0;
 	}
 
